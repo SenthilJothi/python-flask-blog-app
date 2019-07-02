2ac916b59c78a10a2e8655c25c72e542ad8513be
Senthil Jothi <sjothi@akamai.com>
2019-07-02 12:34:14 -0400

    full featured working blog app using python

---
+++ flaskblog/config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Config:
+    SECRET_KEY = 'f3221a053114f20a88087a4fc05169d7'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
